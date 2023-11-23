6159dcd063df9337b1f0d3a4ec571b93b11de584
Surin Lee <leesurin99@gmail.com>
2023-11-23 23:58:23 +0900

    백준 이친수 풀이

---
+++ DP/이친수.java
@@ -0,0 +1,32 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이친수 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		if(n == 1 || n == 2) {
+			System.out.println(1);
+			return;
+		}
+
+		long[] dp = new long[n+1];
+
+		dp[1] = 1;
+		dp[2] = 1;
+
+		dp[3] = 2;
+
+		for(int i = 3; i < n+1; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+
+		System.out.println(dp[n]);
+	}
+}
