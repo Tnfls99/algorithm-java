1b9b19c8bb06d4d38125c7abdbb53b4066495df4
Surin Lee <leesurin99@gmail.com>
2024-01-05 02:08:19 +0900

    백준 dp (더하기 시리즈 1) 풀이

---
+++ DP/더하기1.java
@@ -0,0 +1,30 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 더하기1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[11];
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for(int i = 4; i < 11; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+
+		for(int i = 0; i < t; i++) {
+			int k = Integer.parseInt(br.readLine());
+
+			System.out.println(dp[k]);
+		}
+	}
+}
