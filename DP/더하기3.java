9b8d4759975ec6a7374cafcd39dbcc0bbf163159
Surin Lee <leesurin99@gmail.com>
2024-01-09 01:47:15 +0900

    백준 dp 풀이

---
+++ DP/더하기3.java
@@ -0,0 +1,29 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 더하기3 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		long[] dp = new long[1000001];
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i < 1000001; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+		}
+
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+
+			System.out.println(dp[n]);
+		}
+	}
+}
